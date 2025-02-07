7376e3c4884e11b3a5d9745495727010f0854657
washer <washer8394@gmail.com>
2025-02-06 21:05:48 -0500

    Update Comparator.ts
    asdiJGJGSAGASGHDSAGHASHDGIASG

--- src/lib/Comparator.ts
+++ src/lib/Comparator.ts
@@ -34,18 +34,18 @@ export enum CompareYAxis {
 }
 
 interface InputSet {
-  xValue: number;
-  loadouts: Player[];
-  monster: Monster;
+  xValue: number,
+  loadouts: Player[],
+  monster: Monster,
 }
 
 export interface CompareResult {
-  entries: ChartEntry[];
+  entries: ChartEntry[],
   annotations: {
-    x: ChartAnnotation[];
-    y: ChartAnnotation[];
-  };
-  domainMax: number;
+    x: ChartAnnotation[],
+    y: ChartAnnotation[],
+  },
+  domainMax: number,
 }
 
 export default class Comparator {
@@ -69,12 +69,28 @@ export default class Comparator {
     this.baseMonster = scaleMonster(monster);
     this.xAxis = xAxis;
     this.yAxis = yAxis;
-    this.commonOpts = { disableMonsterScaling: true };
+    this.commonOpts = {
+      disableMonsterScaling: true,
+    };
   }
 
   private* inputsIterator(): Generator<InputSet> {
-    const playerInput = (x: number, alterations: PartialDeep<Player>): InputSet =>
-      ({ xValue: x, loadouts: this.baseLoadouts.map((p) => typedMerge(p, alterations)), monster: this.baseMonster });
+    const monsterInput = (
+      x: number,
+      alterations: PartialDeep<Monster>,
+    ): InputSet => ({
+      xValue: x,
+      loadouts: this.baseLoadouts,
+      monster: typedMerge(this.baseMonster, alterations),
+    });
+    const playerInput = (
+      x: number,
+      alterations: PartialDeep<Player>,
+    ): InputSet => ({
+      xValue: x,
+      loadouts: this.baseLoadouts.map((p) => typedMerge(p, alterations)),
+      monster: this.baseMonster,
+    });
     const skillInput = (x: number, stat: keyof PlayerSkills): InputSet =>
       playerInput(x, { skills: { [stat]: x }, boosts: { [stat]: 0 } });
 
@@ -84,58 +100,85 @@ export default class Comparator {
           yield monsterInput(newDef, { skills: { def: newDef } });
         }
         return;
+
       case CompareXAxis.MONSTER_MAGIC:
         for (let newMagic = this.baseMonster.skills.magic; newMagic >= 0; newMagic--) {
           yield monsterInput(newMagic, { skills: { def: newMagic } });
         }
         return;
+
       case CompareXAxis.MONSTER_HP: {
         for (let newHp = this.baseMonster.skills.hp; newHp >= 0; newHp--) {
-          yield { xValue: newHp, loadouts: this.baseLoadouts, monster: scaleMonsterHpOnly(merge(this.baseMonster, { inputs: { monsterCurrentHp: newHp } })) };
+          yield {
+            xValue: newHp,
+            loadouts: this.baseLoadouts,
+            monster: scaleMonsterHpOnly(
+              merge(this.baseMonster, { inputs: { monsterCurrentHp: newHp } }),
+            ),
+          };
         }
         return;
       }
+
       case CompareXAxis.PLAYER_ATTACK_LEVEL:
         for (let newAttack = 0; newAttack <= 125; newAttack++) {
           yield skillInput(newAttack, 'atk');
         }
         return;
+
       case CompareXAxis.PLAYER_STRENGTH_LEVEL:
         for (let newStrength = 0; newStrength <= 125; newStrength++) {
           yield skillInput(newStrength, 'str');
         }
         return;
+
       case CompareXAxis.PLAYER_DEFENCE_LEVEL:
         for (let newDefence = 0; newDefence <= 125; newDefence++) {
           yield skillInput(newDefence, 'def');
         }
         return;
+
       case CompareXAxis.PLAYER_RANGED_LEVEL:
         for (let newRanged = 0; newRanged <= 125; newRanged++) {
           yield skillInput(newRanged, 'ranged');
         }
         return;
+
       case CompareXAxis.PLAYER_MAGIC_LEVEL:
         for (let newMagic = 0; newMagic <= 125; newMagic++) {
           yield skillInput(newMagic, 'magic');
         }
         return;
+
       case CompareXAxis.STAT_DECAY_RESTORE: {
-        const limit = max(this.baseLoadouts, (l) => max(keys(l.boosts), (k) => Math.abs(l.boosts[k]))) || 0;
+        const limit =
+          max(
+            this.baseLoadouts,
+            (l) => max(keys(l.boosts), (k) => Math.abs(l.boosts[k])),
+          ) || 0;
         for (let restore = 0; restore <= limit; restore++) {
           const restoredLoadouts = this.baseLoadouts.map((p) => {
             const newBoosts: PlayerSkills = {} as PlayerSkills;
             keys(p.boosts).forEach((k) => {
               const boost = p.boosts[k];
               const distFromZero = Math.abs(boost);
-              if (restore >= distFromZero) { newBoosts[k] = 0; } else { newBoosts[k] = Math.sign(boost) * (distFromZero - restore); }
+              if (restore >= distFromZero) {
+                newBoosts[k] = 0;
+              } else {
+                newBoosts[k] = Math.sign(boost) * (distFromZero - restore);
+              }
             });
             return typedMerge(p, { boosts: newBoosts });
           });
-          yield { xValue: restore, loadouts: restoredLoadouts, monster: this.baseMonster };
+          yield {
+            xValue: restore,
+            loadouts: restoredLoadouts,
+            monster: this.baseMonster,
+          };
         }
         return;
       }
+
       default:
         throw new Error(`unimplemented xAxisType ${this.xAxis}`);
     }
@@ -145,15 +188,19 @@ export default class Comparator {
     const res: { [loadout: string]: string | undefined } = {};
     const apply = (resultProvider: (loadout: Player) => string | undefined) =>
       x.loadouts.forEach((l) => { res[l.name] = resultProvider(l); });
-    const forwardCalc = (loadout: Player) => new PlayerVsNPCCalc(loadout, x.monster, this.commonOpts);
-    const reverseCalc = (loadout: Player) => new NPCVsPlayerCalc(loadout, x.monster, this.commonOpts);
+    const forwardCalc = (loadout: Player) =>
+      new PlayerVsNPCCalc(loadout, x.monster, this.commonOpts);
+    const reverseCalc = (loadout: Player) =>
+      new NPCVsPlayerCalc(loadout, x.monster, this.commonOpts);
 
     switch (this.yAxis) {
       case CompareYAxis.PLAYER_DPS:
         apply((l) => forwardCalc(l).getDps().toFixed(DPS_PRECISION));
         break;
       case CompareYAxis.PLAYER_EXPECTED_HIT:
-        apply((l) => forwardCalc(l).getDistribution().getExpectedDamage().toFixed(DPS_PRECISION));
+        apply((l) =>
+          forwardCalc(l).getDistribution().getExpectedDamage().toFixed(DPS_PRECISION),
+        );
         break;
       case CompareYAxis.PLAYER_TTK:
         apply((l) => forwardCalc(l).getTtk()?.toFixed(DPS_PRECISION));
@@ -162,7 +209,9 @@ export default class Comparator {
         apply((l) => reverseCalc(l).getDps().toFixed(DPS_PRECISION));
         break;
       case CompareYAxis.DAMAGE_TAKEN:
-        apply((l) => reverseCalc(l).getAverageDamageTaken()?.toFixed(DPS_PRECISION));
+        apply((l) =>
+          reverseCalc(l).getAverageDamageTaken()?.toFixed(DPS_PRECISION),
+        );
         break;
       case CompareYAxis.PLAYER_MAX_HIT:
         apply((l) => forwardCalc(l).getMax().toString());
@@ -170,6 +219,7 @@ export default class Comparator {
       default:
         throw new Error(`unimplemented yAxisType ${this.yAxis}`);
     }
+
     return res;
   }
 
@@ -207,7 +257,9 @@ export default class Comparator {
       const y = this.getOutput(x);
       for (const k of keys(y)) {
         const f = y[k] ? parseFloat(y[k]!) : 0;
-        if (f > domainMax) { domainMax = f; }
+        if (f > domainMax) {
+          domainMax = f;
+        }
       }
       res.push({ ...y, name: x.xValue });
     }
